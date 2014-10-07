5ce168ee81c7299515f09193bba00c7835d86d49
cliffbeall <beall.3@osu.edu>
2014-10-07 10:12:47 -0400

    modified:   run_mosaik.py Added argument parsing

--- run_mosaik.py
+++ run_mosaik.py
@@ -1,6 +1,19 @@
+"""Run_mosaik.py, a script to run MosaikBuild on a set of microbial
+genomes specified in a file and align them to a set of prebuilt Mosaik queries
+with MosaikAligner. 
+See /Volumes/GriffenLeysLab/Cliff/illumina_strains/genome_fastas.txt
+for example of input file. Input genomes should be processed by concatenating fasta
+files, replacing Ns with Xs and placed in the bact_genomes folder.
+cliffbeall 2014
+"""
+import argparse
 import os
 import subprocess
 import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("genome_input_file", help="file of genomes specified as folder/fasta_file, one per line")
+args = parser.parse_args()
 
 print time.strftime("Start: %a %H:%M", time.localtime())
 
@@ -14,7 +27,7 @@ refdir = '/Volumes/GriffenLeysLab/Cliff/illumina_strains/bact_genomes/'
 queries = os.listdir(querydir)
 
 genomefiles = []
-with open("/Volumes/GriffenLeysLab/Cliff/illumina_strains/genome_fastas.txt", "r") as gffile:
+with open(args.genome_input_file, "r") as gffile:
 	for line in gffile:
 		genomefiles.append(line.rstrip())
 
@@ -26,7 +39,7 @@ for genome in genomefiles:
 
 with open('/Volumes/GriffenLeysLab/Cliff/illumina_strains/mosaik_output_1005.txt', 'w') as stdoutfile:
     for genome in genomefiles:
-    	barglist = [mb, '-fr', refdir + genome, '-oa', refdir + genome[:-3] + '.mkb']
+    	barglist = [mb, '-fr', refdir + genome, '-oa', refdir + genome[:-3] + 'mkb']
     	subprocess.call(barglist, stdout=stdoutfile, stderr=subprocess.STDOUT)
         outdir = '/Volumes/GriffenLeysLab/Cliff/illumina_strains/mosaik_run3/' + shortnames[genome]
         os.mkdir(outdir)
